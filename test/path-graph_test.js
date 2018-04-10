a985f54f79c1c690d8570af59ad90e3b2a41e0f7
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-04-10 15:19:19 +0100

    Add gap filling function that finds closest points

--- test/path-graph_test.js
+++ test/path-graph_test.js
@@ -132,6 +132,7 @@ describe('path graphs', function () {
     p.add({type: 'line', start: [0, 0], end: [1, 0]})
     p.add({type: 'line', start: [1, 0], end: [0, 0]})
     p.add({type: 'line', start: [0, 0], end: [1, 0]})
+    p.traverse()
     expect(p.length).to.equal(1)
   })
 
@@ -162,10 +163,10 @@ describe('path graphs', function () {
   it('should be able to fill gaps', function () {
     p = new PathGraph(true, true)
 
-    p.add({type: 'line', start: [0, 0], end: [1, 0]})
-    p.add({type: 'line', start: [1.0001, 0], end: [1, 1]})
-    p.add({type: 'line', start: [1.0001, 1], end: [0, 1]})
-    p.add({type: 'line', start: [0, 1.0001], end: [0, 0]})
+    p.add({type: 'line', start: [0, 0], end: [1.0001, 0]})
+    p.add({type: 'line', start: [1, 0], end: [1.0001, 1]})
+    p.add({type: 'line', start: [1, 1], end: [0, 1.0001]})
+    p.add({type: 'line', start: [0, 1], end: [0, 0]})
 
     expect(p.traverse()).to.eql([
       {type: 'line', start: [0, 0], end: [1, 0]},
@@ -178,10 +179,10 @@ describe('path graphs', function () {
   it('should be able to fill gaps with a specified gap distance', function () {
     p = new PathGraph(true, 0.0011)
 
-    p.add({type: 'line', start: [0, 0], end: [1, 0]})
-    p.add({type: 'line', start: [1.001, 0], end: [1, 1]})
-    p.add({type: 'line', start: [1.001, 1], end: [0, 1]})
-    p.add({type: 'line', start: [0, 1.001], end: [0, 0]})
+    p.add({type: 'line', start: [0, 0], end: [1.001, 0]})
+    p.add({type: 'line', start: [1, 0], end: [1.001, 1]})
+    p.add({type: 'line', start: [1, 1], end: [0, 1.001]})
+    p.add({type: 'line', start: [0, 1], end: [0, 0]})
 
     expect(p.traverse()).to.eql([
       {type: 'line', start: [0, 0], end: [1, 0]},
@@ -195,15 +196,15 @@ describe('path graphs', function () {
     p = new PathGraph(true, 0.06)
 
     p.add({type: 'line', start: [0, 0], end: [1, 0]})
-    p.add({type: 'line', start: [1.01, 0], end: [1.02, 0]})
-    p.add({type: 'line', start: [1.02, 0], end: [1.03, 0.01]})
-    p.add({type: 'line', start: [1.03, 0.01], end: [2, 2]})
+    p.add({type: 'line', start: [1.01, 0], end: [1.03, 0]})
+    p.add({type: 'line', start: [1.03, 0], end: [1.04, 0.01]})
+    p.add({type: 'line', start: [1.04, 0.01], end: [2, 2]})
 
     expect(p.traverse()).to.eql([
-      {type: 'line', start: [0, 0], end: [1, 0]},
-      {type: 'line', start: [1, 0], end: [1.02, 0]},
-      {type: 'line', start: [1.02, 0], end: [1.03, 0.01]},
-      {type: 'line', start: [1.03, 0.01], end: [2, 2]},
+      {type: 'line', start: [0, 0], end: [1.01, 0]},
+      {type: 'line', start: [1.01, 0], end: [1.03, 0]},
+      {type: 'line', start: [1.03, 0], end: [1.04, 0.01]},
+      {type: 'line', start: [1.04, 0.01], end: [2, 2]},
     ])
   })
 })
